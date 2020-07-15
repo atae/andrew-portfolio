9bff74a949f4fafcfa57081d693918c48158b611
Andrew Tae <andrewtae368@gmail.com>
2020-07-14 17:39:49 -0700

    Updates

---
+++ precache-manifest.ca55fb0f9ffbcc2077d82b9fb0d456c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a05243f9e74ba2c1c4de1fad07d366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "051665b1ae68652d16f5",
+    "url": "/static/css/main.08af27e9.chunk.css"
+  },
+  {
+    "revision": "d0b2cb881be76171cdc0",
+    "url": "/static/js/2.ef1a94f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef1a94f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "051665b1ae68652d16f5",
+    "url": "/static/js/main.fc35cef8.chunk.js"
+  },
+  {
+    "revision": "a63865d534d94be5fd0d",
+    "url": "/static/js/runtime-main.02f450b7.js"
+  },
+  {
+    "revision": "e950cb73622985785a5e2897dcba2529",
+    "url": "/static/media/Andrew-Photo-Cropped.e950cb73.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8bbb41e9939224789ea5db5e49cb50c4.js"
+  "/precache-manifest.ca55fb0f9ffbcc2077d82b9fb0d456c0.js"
 );
 
 self.addEventListener('message', (event) => {
